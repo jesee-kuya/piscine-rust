1bb8d1eb7f81b01a764e3ada5304e0365cd52b96
jkuya <kuyajesee@proton.me>
2025-04-03 02:57:26 +0300

    Add vec_function to compute values and logarithms from a vector of integers with unit tests

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -15,6 +15,17 @@ pub fn str_function(a: String) -> (String, String) {
     (a, exp)
 }
 
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    let mut val = vec![];
+    let mut log = vec![];
+    for num in b {
+        let res = nbr_function(num);
+        val.push(num);
+        log.push(res.2);
+    }
+    (val, log)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,9 +33,11 @@ mod tests {
     #[test]
     fn it_works() {
         let c = 0;
+        let b = vec![1, 2, 4, 5];
         let a = "1 2 4 5 6".to_owned();
 
         assert_eq!(nbr_function(c), (0, 1.0, f64::NEG_INFINITY));
-        assert_eq!(str_function(a), ("1 2 4 5 6".to_string(), "2.718281828459045 7.38905609893065 54.598150033144236 148.4131591025766 403.4287934927351".to_string()))
+        assert_eq!(str_function(a), ("1 2 4 5 6".to_string(), "2.718281828459045 7.38905609893065 54.598150033144236 148.4131591025766 403.4287934927351".to_string()));
+        assert_eq!(vec_function(b), (vec![1, 2, 4, 5], vec![0.0, 0.6931471805599453, 1.3862943611198906, 1.6094379124341003]))
     }
 }
