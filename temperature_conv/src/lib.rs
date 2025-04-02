ff46d6331ea4c1e82b36a06c172b9e7af1499836
jkuya <kuyajesee@proton.me>
2025-04-02 10:15:05 +0300

    Fix fahrenheit_to_celsius formula and add additional test case

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -3,7 +3,7 @@ pub fn celsius_to_fahrenheit(c: f64) -> f64 {
 }
 
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) / 9.0/5.0
+    (f - 32.0) / (9.0/5.0)
 }
 
 #[cfg(test)]
@@ -14,5 +14,6 @@ mod tests {
     fn it_works() {
         assert_eq!(celsius_to_fahrenheit(0.0), 32.0);
         assert_eq!(fahrenheit_to_celsius(32.0), 0.0);
+        assert_eq!(fahrenheit_to_celsius(20.0), -6.666666666666666)
     }
 }
