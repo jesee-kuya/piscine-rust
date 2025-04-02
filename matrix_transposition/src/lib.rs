f26dcd43c24fff2b8f13c85e8dc0a14402563432
jkuya <kuyajesee@proton.me>
2025-04-02 15:48:13 +0300

    Add matrix transposition functionality with tests

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+#[derive(PartialEq)]
+#[derive(Eq)]
+pub struct Matrix ((i32, i32), (i32, i32));
+
+pub fn transpose(m: Matrix) -> Matrix {
+    Matrix((m.0.0, m.1.0), (m.0.1, m.1.1))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(transpose(Matrix((1, 3), (4, 5))), Matrix((1, 4), (3, 5)));
+    }
+}
