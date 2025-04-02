ce745f3583f11b45b0fbd6f3ba5ab13ada2c61eb
jkuya <kuyajesee@proton.me>
2025-04-03 00:15:49 +0300

    Add is_ascii, contains, and split_at functions with unit tests

--- string_literals/src/lib.rs
+++ string_literals/src/lib.rs
@@ -2,6 +2,24 @@ pub fn is_empty(v: &str) -> bool {
     v.chars().count() == 0
 }
 
+pub fn is_ascii(v: &str) -> bool {
+    for c in v.chars() {
+        let n = c as u32;
+        if n > 177 {
+            return false
+        }
+    }
+    true
+}
+
+pub fn contains(v: &str, pat: &str) -> bool {
+    v.contains(pat)
+}
+
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    (&v[..index], &v[index..])
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +27,8 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(is_empty(""), true);
+        assert_eq!(is_ascii("rust"), true);
+        assert_eq!(contains("rust", "ru"), true);
+        assert_eq!(split_at("rust", 2), ("ru", "st"));
     }
 }
