a576dac53192f389671c22c9431154153aeb5811
jkuya <kuyajesee@proton.me>
2025-04-02 22:52:25 +0300

    Add str_len function with unit tests for string length calculation

---
+++ borrow/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn str_len(s: &str ) -> usize {
+    s.to_string().chars().count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let s = "hello";
+        let s1 = "camelCase".to_string();
+        let s2 = "olá!";
+        assert_eq!(str_len(s), 5);
+        assert_eq!(str_len(&s1), 9);
+        assert_eq!(str_len(s2), 4);
+    }
+}
