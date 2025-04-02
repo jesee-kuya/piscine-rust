8d3395f3bd80aad28b8b544909e89f532ece25a1
jkuya <kuyajesee@proton.me>
2025-04-03 00:21:30 +0300

    Add find function to locate the index of a character in a string with unit tests

--- string_literals/src/lib.rs
+++ string_literals/src/lib.rs
@@ -20,6 +20,17 @@ pub fn split_at(v: &str, index: usize) -> (&str, &str) {
     (&v[..index], &v[index..])
 }
 
+pub fn find(v: &str, pat: char) -> usize {
+    let mut n: usize = 0;
+    for c in v.chars() {
+        if c == pat {
+            return n
+        }
+        n += 1;
+    }
+    n
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +41,6 @@ mod tests {
         assert_eq!(is_ascii("rust"), true);
         assert_eq!(contains("rust", "ru"), true);
         assert_eq!(split_at("rust", 2), ("ru", "st"));
+        assert_eq!(find("rust", 'u'), 1)
     }
 }
