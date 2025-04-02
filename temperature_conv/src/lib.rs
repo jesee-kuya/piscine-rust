eb533c8cd4228cdd742c0672e79ab3e515c70ae5
jkuya <kuyajesee@proton.me>
2025-04-02 15:53:33 +0300

    Fix precision in temperature conversion test assertions

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -12,7 +12,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(celsius_to_fahrenheit(0.0), 32.0);
+        assert_eq!(celsius_to_fahrenheit(0.0), 32.00);
         assert_eq!(fahrenheit_to_celsius(32.0), 0.0);
         assert_eq!(fahrenheit_to_celsius(20.0), -6.666666666666666)
     }
