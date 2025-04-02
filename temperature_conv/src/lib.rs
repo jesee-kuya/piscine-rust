9196e16377baf5989a5505b9339b463c867ab8f0
jkuya <kuyajesee@proton.me>
2025-04-02 09:57:53 +0300

    Add temperature conversion functions with tests

---
+++ temperature_conv/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0/5.0) + 32.0
+}
+
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * 5.0/9.0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(celsius_to_fahrenheit(0.0), 32.0);
+        assert_eq!(fahrenheit_to_celsius(32.0), 0.0);
+    }
+}
