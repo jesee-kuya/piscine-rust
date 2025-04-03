f9d99633ceb00c19b99b1935f50f2665579d33bd
jkuya <kuyajesee@proton.me>
2025-04-03 04:47:08 +0300

    Add do_operations function to perform arithmetic operations on strings with unit tests

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -24,6 +24,38 @@ pub fn delete_and_backspace(s: &mut String) {
     *s = result;
 }
 
+pub fn do_operations(v: &mut [String]) {
+    for word in v {
+        let mut eq = String::new();
+        let mut hld: i32 = 0;
+        
+        for c in word.chars() {
+            if c == '+' {
+                eq = "add".to_string();
+                continue;
+            }else if c == '-' {
+                eq = "subtract".to_string();
+                continue;
+            }
+
+            if eq == "add" {
+                let n: i32 = c as i32 - '0' as i32;
+                hld += n;
+                continue;
+            } else if eq == "subtract" {
+                let n: i32 = c as i32 - '0' as i32;
+                hld -= n;
+                continue;
+            }
+
+            hld = (hld * 10) + (c as i32 - '0' as i32);
+        }
+        *word = hld.to_string();
+    }
+}
+
+
+
 
 
 #[cfg(test)]
@@ -34,7 +66,15 @@ mod tests {
     fn it_works() {
         let mut a = "bpp--o+er+++sskroi-++lcw".to_owned();
         delete_and_backspace(&mut a);
+        let mut b = [
+        "2+2".to_owned(),
+        "3+2".to_owned(),
+        "10-3".to_owned(),
+        "5+5".to_owned(),
+        ];
+        do_operations(&mut b);
 
         assert_eq!(a, "borrow".to_owned());
+        assert_eq!(b, ["4".to_string(), "5".to_string(), "7".to_string(), "10".to_string()]);
     }
 }
