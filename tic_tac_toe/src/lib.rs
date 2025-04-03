1b8a2a1226e6269e1f5eefca6976a8281a3fcf2b
jkuya <kuyajesee@proton.me>
2025-04-03 17:14:40 +0300

    Add tic_tac_toe function to determine game outcome based on current board state

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -14,6 +14,16 @@ pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
     (player == table[0][1] && player == table[1][1] && player == table[2][1]) ||
     (player == table[0][2] && player == table[1][2] && player == table[2][2])
 }
+
+pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
+    if diagonals('X', table) || horizontal('X', table) || vertical('X', table) {
+        return "player X won".to_string();
+    } else if diagonals('O', table) || horizontal('O', table) || vertical('O', table) {
+        return "player O won".to_string();
+    } else {
+        return "tie".to_string();
+    }
+}
     
 
 
@@ -29,5 +39,9 @@ mod tests {
         assert_eq!(diagonals('X', diag), true);
         assert_eq!(horizontal('X', horz), false);
         assert_eq!(vertical('O', vert), true);
+        assert_eq!(tic_tac_toe([['O', 'X', 'O'], ['O', 'P', 'X'], ['X', '#', 'X']]), "tie".to_string());
+        assert_eq!(tic_tac_toe(vert), "player O won".to_string());
+        assert_eq!(tic_tac_toe(diag), "player X won".to_string());
+
     }
 }
