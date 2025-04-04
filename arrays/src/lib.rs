9427da8694f9d026bdf9b6d9f7c3807d7ca90693
jkuya <kuyajesee@proton.me>
2025-04-05 01:48:58 +0300

    feat: implement sum

--- arrays/src/lib.rs
+++ arrays/src/lib.rs
@@ -2,6 +2,10 @@ pub fn thirtytwo_tens() -> [i32; 32] {
     [10; 32]
 }
 
+pub fn sum(a: &[i32]) -> i32 {
+    a.iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +14,12 @@ mod tests {
     fn test_thirtytwo_tens() {
         let arr = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
         assert_eq!(thirtytwo_tens(), arr);
+    }
+
+    fn test_sum() {
+        let a = (1..=10)._;
+        let b = [_];
+        assert_eq!(sum(a), 55);
+        assert_eq!(sum[b], 50);
     }
 }
