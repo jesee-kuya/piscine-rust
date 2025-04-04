696df89f60fbf50b481146cef1b9fa2e64c9f217
jkuya <kuyajesee@proton.me>
2025-04-04 22:11:37 +0300

    feat: implement methods of CIrcle

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -1,10 +1,10 @@
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Circle {
     pub center: Point,
     pub radius: f64,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Point(pub f64, pub f64);
 
 impl Point {
@@ -13,16 +13,64 @@ impl Point {
     }
 }
 
+impl Circle {
+    fn new(p1: f64, p2: f64, p3: f64) -> Self {
+        Self {
+            center: Point(p1, p2),
+            radius: p3,
+        }
+    }
+
+    fn diameter(&self) -> f64 {
+        self.radius * 2.0
+    }
+
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+
+    fn intersect(&self, other: Circle) -> bool {
+        let d = self.center.distance(other.center);
+        self.radius - other.radius <= d && self.radius + other.radius >= d
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn distance() {
+    fn test_distance() {
 
         let point_a = Point(1.0, 1.0);
         let point_b = Point(0.0, 0.0);
 
         assert_eq!(point_a.distance(point_b), 1.4142135623730951);
     }
+
+    fn test_new() {
+        let circle = Circle::new(500.0, 500.0, 150.0);
+        assert_eq!(circle, Circle { center: Point(500.0, 500.0), radius: 150.0 });
+    }
+
+    fn test_diameter() {
+        let circle = Circle::new(500.0, 500.0, 150.0);
+        assert_eq!(circle.diameter(), 300.0);
+    }
+
+    fn test_area() {
+        let circle = Circle::new(500.0, 500.0, 150.0);
+        assert_eq!(circle.area(), 70685.83470577035);
+    }
+
+    fn test_intersect() {
+        let circle = Circle::new(500.0, 500.0, 150.0);
+        let circle1 = Circle {
+            center: Point(80.0, 115.0),
+            radius: 30.0,
+        };
+
+        assert_eq!(circle.intersect(circle1), false)
+    }
+
 }
