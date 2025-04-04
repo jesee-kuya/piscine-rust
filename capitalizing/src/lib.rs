32a2896ae86b35e264cb1074f0bb684f3b7340e4
jkuya <kuyajesee@proton.me>
2025-04-05 02:40:00 +0300

    feat: declarea function that title cases a string

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -8,6 +8,19 @@ pub fn capitalize_first(input: &str) -> String {
     }
 }
 
+pub fn title_case(input: &str) -> String {
+    let words = input.split_whitespace();
+    let mut s = String::from("");
+
+    for word in words {
+        s.push_str(&capitalize_first(word));
+        s.push(' ');
+    }
+
+    s = s.trim().to_string();
+    s
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -15,5 +28,6 @@ mod tests {
     #[test]
     fn test_capitalize_first() {
         assert_eq!(capitalize_first("joe is missing"), "Joe is missing".to_string());
+        assert_eq!(title_case("jill is leaving A"), String::from("Jill Is Leaving A"));
     }
 }
