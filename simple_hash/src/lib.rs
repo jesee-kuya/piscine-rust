5f2b1dae138489dd8da9968c1db71e23a62db242
jkuya <kuyajesee@proton.me>
2025-04-05 04:34:27 +0300

    feat: handle array gracefully

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -2,13 +2,13 @@ use std::collections::HashMap;
 
 pub const SENTENCE: &str = "this is a very basic sentence with only a few repetitions. once again this is very basic but it should be enough for basic tests";
 
-pub fn word_frequency_counter<'a>(words: &'a Vec<&'a str>) -> HashMap<&'a str, usize> {
+pub fn word_frequency_counter<'a>(words: &'a [&'a str]) -> HashMap<&'a str, usize> {
     let mut hmap = HashMap::new();
 
     for &word in words { 
         *hmap.entry(word).or_insert(0) += 1; 
     }
-    
+
     hmap
 }
 
