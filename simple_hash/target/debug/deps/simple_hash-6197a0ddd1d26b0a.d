800e785e54c89d27c0cf78eda9b115e72b2a5059
jkuya <kuyajesee@proton.me>
2025-04-05 04:22:02 +0300

    refactor:move functions to its own folder

--- simple_hash/target/debug/deps/simple_hash-6197a0ddd1d26b0a.d
+++
@@ -1,7 +0,0 @@
-/home/jkuya/piscine-rust/edit_distance/simple_hash/target/debug/deps/libsimple_hash-6197a0ddd1d26b0a.rmeta: src/lib.rs
-
-/home/jkuya/piscine-rust/edit_distance/simple_hash/target/debug/deps/libsimple_hash-6197a0ddd1d26b0a.rlib: src/lib.rs
-
-/home/jkuya/piscine-rust/edit_distance/simple_hash/target/debug/deps/simple_hash-6197a0ddd1d26b0a.d: src/lib.rs
-
-src/lib.rs:
--- simple_hash/target/debug/deps/simple_hash-ad6be6f68a910053.d
+++
@@ -1,5 +0,0 @@
-/home/jkuya/piscine-rust/edit_distance/simple_hash/target/debug/deps/simple_hash-ad6be6f68a910053: src/lib.rs
-
-/home/jkuya/piscine-rust/edit_distance/simple_hash/target/debug/deps/simple_hash-ad6be6f68a910053.d: src/lib.rs
-
-src/lib.rs:
