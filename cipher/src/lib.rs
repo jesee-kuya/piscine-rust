ba72eff8bb0c41a3e6b6d13c4c71d0a9a38836df
jkuya <kuyajesee@proton.me>
2025-04-09 02:32:49 +0300

    feat: implement cipher

---
+++ cipher/src/lib.rs
@@ -0,0 +1,31 @@
+#[derive(Debug, PartialEq)]
+pub struct CipherError {
+    pub expected: String,
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
+    let res = atbash(original);
+
+    match res == ciphered {
+        true => Ok(()),
+        false => Err(CipherError{
+            expected: res,
+        })
+    }
+}
+
+pub fn atbash(s: &str) -> String {
+    let s1 = String::from("");
+    for c in s.chars() {
+        if c >= 'a' && c <= 'z' {
+            let n = (122 - c as u8) + 97;
+            s1.push(n as char)
+        } else if c >= 'A' && c <= 'Z' {
+            let n = (90 - c as u8) + 65;
+            s1.push(n as char)
+        } else {
+            s1.push(c)
+        }
+    }
+    s1
+}
