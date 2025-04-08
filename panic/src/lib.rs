a7ba355dda04511d80bf4304958d1051496bee29
jkuya <kuyajesee@proton.me>
2025-04-08 03:46:36 +0300

    feat: add open_file function and initial tests in panic module

---
+++ panic/src/lib.rs
@@ -0,0 +1,16 @@
+use::std::fs::File;
+
+pub fn open_file(s: &str) -> File {
+    File::open(s).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
