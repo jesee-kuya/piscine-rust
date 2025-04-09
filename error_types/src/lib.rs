449b98f7e531a0f860f0327739e5589c765981ab
jkuya <kuyajesee@proton.me>
2025-04-09 21:40:35 +0300

    feat: implement Form and FormError structs with validation logic

---
+++ error_types/src/lib.rs
@@ -0,0 +1,89 @@
+use std::collections::HashMap;
+use chrono::{ Utc };
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError {
+    pub form_values: HashMap<String, String>,
+    pub date: String,
+    pub err: String,
+}
+
+impl FormError {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        let mut map = HashMap::new();
+        map.insert(format!("{field_name}"), format!("{field_value}"));
+        let utc = format!("{}",Utc::now().format("%Y-%m-%d %H:%M:%S"));
+
+        Self {
+            form_values: map,
+            date: utc,
+            err: err.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    pub name: String,
+    pub password: String,
+}
+
+impl Form {
+    pub fn new(name: &str, password: &str) -> Self {
+        Self {
+            name: format!("{name}"),
+            password: format!("{password}"),
+        }
+    }
+
+    pub fn validate(&self) -> Result<(), FormError> {
+        let has_alphanum = self.password.chars().any(|c| c.is_alphanumeric());
+        let has_numbers = self.password.chars().any(|c| c.is_numeric()); 
+        let has_symbol = self.password.chars().any(|c| !c.is_alphanumeric() && !c.is_whitespace());
+        if self.name.trim().is_empty() {
+            Err(FormError::new("name", format!("{}",self.name), "Username is empty"))
+        } else if self.password.len() < 8{
+            Err(FormError::new("password", format!("{}", self.password), "Password should be at least 8 characters long"))
+        } else if !(has_alphanum && has_symbol && has_numbers) {
+            Err(FormError::new("password", format!("{}", self.password), "Password should be a combination of ASCII numbers, letters and symbols"))
+        }else {
+            Ok(())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut form_output = Form {
+            name: "Lee".to_owned(),
+            password: "qwqwsa1dty_".to_owned(),
+        };
+        assert_eq!(form_output.validate(), Ok(()));
+
+        form_output.name = "".to_owned();
+        let result = form_output.validate();
+        let err = result.unwrap_err();
+        assert_eq!(err.err, "Username is empty");
+
+        form_output.name = "as".to_owned();
+        form_output.password = "dty_1".to_owned();
+        let result = form_output.validate();
+        let err = result.unwrap_err();
+        assert_eq!(err.err, "Password should be at least 8 characters long");
+
+        form_output.password = "asdasASd(_".to_owned();
+        let result = form_output.validate();
+        let err = result.unwrap_err();
+        assert_eq!(err.err, "Password should be a combination of ASCII numbers, letters and symbols");
+
+        form_output.password = "asdasASd123SA".to_owned();
+        let result = form_output.validate();
+        let err = result.unwrap_err();
+        assert_eq!(err.err, "Password should be a combination of ASCII numbers, letters and symbols");
+
+    }
+}
