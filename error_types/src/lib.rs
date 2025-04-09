3e7fd9c90363c28516a1e45e1906bcac0892b8bd
jkuya <kuyajesee@proton.me>
2025-04-09 22:45:39 +0300

    refactor: update FormError to use borrowed field names and simplify string handling

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -1,24 +1,23 @@
 pub use std::collections::HashMap;
-pub use chrono::Utc;
+pub use chrono::{ Utc };
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct FormError {
-    pub form_values: HashMap<String, String>,
+pub struct FormError<'a> {
+    pub form_values: HashMap<&'a str, String>,
     pub date: String,
     pub err: String,
 }
 
-impl FormError {
-    pub fn new(field_name: &str, field_value: String, err: &str) -> Self {
+impl FormError<'_> {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
         let mut map = HashMap::new();
-        map.insert(field_name.to_string(), field_value);
-
-        let utc = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        map.insert(field_name, field_value.to_string());
+        let utc = format!("{}",Utc::now().format("%Y-%m-%d %H:%M:%S"));
 
         Self {
             form_values: map,
             date: utc,
-            err: err.to_string(),
+            err: err.to_owned(),
         }
     }
 }
@@ -32,8 +31,8 @@ pub struct Form {
 impl Form {
     pub fn new(name: &str, password: &str) -> Self {
         Self {
-            name: name.to_string(),
-            password: password.to_string(),
+            name: format!("{name}"),
+            password: format!("{password}"),
         }
     }
 
@@ -41,18 +40,13 @@ impl Form {
         let has_alphanum = self.password.chars().any(|c| c.is_alphanumeric());
         let has_numbers = self.password.chars().any(|c| c.is_numeric()); 
         let has_symbol = self.password.chars().any(|c| !c.is_alphanumeric() && !c.is_whitespace());
-
         if self.name.trim().is_empty() {
-            Err(FormError::new("name", self.name.clone(), "Username is empty"))
-        } else if self.password.len() < 8 {
-            Err(FormError::new("password", self.password.clone(), "Password should be at least 8 characters long"))
+            Err(FormError::new("name", format!("{}",self.name), "Username is empty"))
+        } else if self.password.len() < 8{
+            Err(FormError::new("password", format!("{}", self.password), "Password should be at least 8 characters long"))
         } else if !(has_alphanum && has_symbol && has_numbers) {
-            Err(FormError::new(
-                "password",
-                self.password.clone(),
-                "Password should be a combination of ASCII numbers, letters and symbols",
-            ))
-        } else {
+            Err(FormError::new("password", format!("{}", self.password), "Password should be a combination of ASCII numbers, letters and symbols"))
+        }else {
             Ok(())
         }
     }
