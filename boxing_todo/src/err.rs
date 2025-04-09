0aaddc2a6982885206af259fafd1529ef2138fbe
jkuya <kuyajesee@proton.me>
2025-04-10 00:45:00 +0300

    feat: add custom error types for parsing and reading todo files

---
+++ boxing_todo/src/err.rs
@@ -0,0 +1,40 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+#[derive(Debug)]
+pub enum ParseErr {
+    Empty,
+    Malformed(Box<dyn Error>),
+}
+
+impl Display for ParseErr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "Failed to parse todo file")
+    }
+}
+
+impl Error for ParseErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            ParseErr::Malformed(e) => Some(e.as_ref()),
+            ParseErr::Empty => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ReadErr {
+    pub child_err: Box<dyn Error>,
+}
+
+impl Display for ReadErr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "Failed to read todo file")
+    }
+}
+
+impl Error for ReadErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(self.child_err.as_ref())
+    }
+}
