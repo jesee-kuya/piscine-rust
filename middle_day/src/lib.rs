6a86e958bce6926d3d6bec8226fd44efe74710cf
jkuya <kuyajesee@proton.me>
2025-04-10 15:21:59 +0300

    chore:leap year

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -2,10 +2,11 @@ use chrono::{NaiveDate, Datelike};
 pub use chrono::Weekday as wd;
 
 pub fn middle_day(year: i32) -> Option<wd> {
-    let is_leap_year = NaiveDate::from_ymd_opt(year, 2, 29).is_some();
-    let total_days = if is_leap_year { 366 } else { 365 };
+    if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) {
+        return None
+    }
     
-    let middle_day = (total_days + 1) / 2;
+    let middle_day = 183;
     let date = NaiveDate::from_yo_opt(year, middle_day)?;
     
     Some(date.weekday())
