4300eb8af9aa3dd9d7d85ce9fddb44813bc02bbf
jkuya <kuyajesee@proton.me>
2025-04-12 17:23:46 +0300

    feat: create lib.rs

---
+++ ordinal/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn num_to_ordinal(x: u32) -> String {
+    let mut s = x.to_string();
+
+    match x % 10 {
+        1 => s.push_str("st"),
+        2 => s.push_str("nd"),
+        3 => s.push_str("rd"),
+        _ => s.push_str("th"),
+    }
+    s
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
