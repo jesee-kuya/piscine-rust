653426fb8a51a20dfbe4359dcd590cfa31591a21
jkuya <kuyajesee@proton.me>
2025-04-12 21:26:05 +0300

    update lib.rs

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -3,15 +3,17 @@ pub fn pig_latin(text: &str) -> String {
     let mut start = String::new();
     let mut found_vowel = false;
     let mut last = text.chars().next();
+    let mut count = 0;
 
     for c in text.chars() {
-        if (!found_vowel && !is_vowel(c)) || c == 'u' && last == Some('q') {
+        if (!found_vowel && !is_vowel(c)) || (((c == 'u' && last == Some('q')) &&  !found_vowel) && count >= 2) {
             end.push(c);
         } else {
             found_vowel = true;
             start.push(c);
         }
-        last = Some(c)
+        last = Some(c);
+        count += 1;
     }
 
     start + &end + "ay"
@@ -32,5 +34,7 @@ mod tests {
         assert_eq!(pig_latin(&String::from("apple")), "appleay".to_string());
         assert_eq!(pig_latin(&String::from("hello")), "ellohay".to_string());
         assert_eq!(pig_latin(&String::from("square")), "aresquay".to_string());
+        assert_eq!(pig_latin(&String::from("queen")), "ueenqay".to_string());
+        assert_eq!(pig_latin(&String::from("equal")), "equalay".to_string());
     }
 }
