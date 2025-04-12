94e2a59646c680be38814aef91034ae657337de3
jkuya <kuyajesee@proton.me>
2025-04-12 17:59:11 +0300

    update main.rs

--- pangram/src/main.rs
+++ pangram/src/main.rs
@@ -5,5 +5,5 @@ fn main() {
         "{}",
         is_pangram("the quick brown fox jumps over the lazy dog!")
     );
-    println!("{}", is_pangram("this is not a pangram!"));
+    println!("{}", is_pangram("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich."));
 }
