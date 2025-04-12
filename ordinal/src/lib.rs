9e1752a337a2f2cb08e0d8b689e5b15fbecbaa19
jkuya <kuyajesee@proton.me>
2025-04-12 17:28:19 +0300

    update lib.rs

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,12 +1,17 @@
 pub fn num_to_ordinal(x: u32) -> String {
     let mut s = x.to_string();
 
-    match x % 10 {
-        1 => s.push_str("st"),
-        2 => s.push_str("nd"),
-        3 => s.push_str("rd"),
-        _ => s.push_str("th"),
+    if let 11..=13 = x % 100{
+        s.push_str("th");
+    } else {
+        match x % 10 {
+            1 => s.push_str("st"),
+            2 => s.push_str("nd"),
+            3 => s.push_str("rd"),
+            _ => s.push_str("th"),
+        }
     }
+
     s
 }
 
