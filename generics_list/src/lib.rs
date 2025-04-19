c656a7d297eee9813ef496337c70a6167aded2af
jkuya <kuyajesee@proton.me>
2025-04-19 21:10:38 +0300

    implement generics list

---
+++ generics_list/src/lib.rs
@@ -0,0 +1,41 @@
+#[derive(Clone, Debug)]
+pub struct List<T> {
+    pub head: Option<Node<T>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Node<T> {
+    pub value: T,
+    pub next: Option<Box<Node<T>>>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> List<T> {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, value: T) {
+        let old_head = self.head.take();
+        let new_node = Node {
+            value,
+            next: old_head.map(Box::new),
+        };
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) {
+        if let Some(node) = self.head.take() {
+            self.head = node.next.map(|boxed| *boxed);
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        let mut count = 0;
+        let mut current = self.head.as_ref();
+        while let Some(node) = current {
+            count += 1;
+            current = node.next.as_ref().map(|boxed| &**boxed);
+        }
+        count
+    }
+}
