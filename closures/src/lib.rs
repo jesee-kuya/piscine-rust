f23fc0e71b21e0e130bfb36edf2d268cc5c91c89
jkuya <kuyajesee@proton.me>
2025-04-25 04:26:56 +0300

    add Cargo.toml and Cargo.lock for closures package initialization with first_fifty_even_square function and tests

---
+++ closures/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn first_fifty_even_square() -> Vec<i32> {
+    (0..)
+        .filter(|x| x % 2 == 0)
+        .map(|x| x * x)
+        .take(50)
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let v1 = first_fifty_even_square();
+        assert_eq!(v1.len(), 50);
+    }
+}
