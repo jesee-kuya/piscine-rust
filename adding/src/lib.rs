a3bd86828583b61afdf83564e68ee75983f4f15c
jkuya <kuyajesee@proton.me>
2025-04-26 19:32:32 +0300

    add curry function with tests for closure-based addition

---
+++ adding/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn add_curry(num: i32) -> impl Fn(i32) -> i32 {
+    move |x| num + x
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let add10 = add_curry(-10);
+        let add20 = add_curry(2066);
+        let add30 = add_curry(300000);
+       
+        assert_eq!(add10(5), -5);
+        assert_eq!(add20(195), 2261);
+        assert_eq!(add30(5696), 305696);
+    }
+}
